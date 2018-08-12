8430e5b5c25bed1d3a4990f8aa6cb88e41fbf733
shakogele <shakogele@gmail.com>
2018-08-12 17:42:52 +0400

    Error Catcher

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import classes from './App.css';
 import Person from './Person/Person';
+import ErrorCatcher from './ErrorCatcher/ErrorCatcher';
 
 class App extends Component {
   state = {
@@ -63,12 +64,12 @@ class App extends Component {
       persons = (
           <div>
               {this.state.persons.map((person, index) => {
-                  return <Person
+                  return <ErrorCatcher key={person.id}><Person
                     click={this.deletePersonHandler.bind(this, index)}
                     changed={(event) => {this.changeNameHandler(event, index); }}
                     name={person.name}
                     age={person.age}
-                    key={person.id}/>
+                    key={person.id}/></ErrorCatcher>
                 })
               }
           </div>
---
+++ src/ErrorCatcher/ErrorCatcher.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+class ErrorCatcher extends Component {
+    state = {
+        hasError: false,
+        errorMessage: ''
+    };
+
+    componentDidCatch = (error, info) => {
+        this.setState({
+          hasError: true,
+          errorMessage: error
+        });
+    };
+
+    render () {
+      if (this.state.hasError){
+        return <h1>{this.state.errorMessage}</h1>
+      } else {
+        return this.props.children;
+      }
+    }
+}
+
+export default ErrorCatcher;
